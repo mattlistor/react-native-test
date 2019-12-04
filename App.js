07deef107a5037f0c1cad49419def3c3fe6f0d90
mattlistor <mattlistor@gmail.com>
2019-12-04 00:45:48 -0500

    here

--- App.js
+++ App.js
@@ -11,24 +11,27 @@ class App extends Component {
       <ImageBackground style={{width: '100%', height: '100%'}} source={{uri: 'https://images.unsplash.com/photo-1575081151297-19c23575e91b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}}>
         
           <View style={{ marginHorizontal: 20, marginVertical: 50, marginTop: 600}} >
-            <Text style={{ color: "white", fontSize: 50, fontWeight: "bold", marginBottom: 20}}>Water</Text>
-            <ScrollView horizontal= {true} decelerationRate={0} snapToInterval={110} snapToAlignment={"left"} style={{ backgroundColor: "transparent"}}>
-              {/* <Button onPress={() => {this.setState({duck: !this.state.duck})}} title="Press Me"/> */}
-              <Image style={{width: 100, height: 100, marginRight: 10}} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
-              <Image style={{width: 100, height: 100, marginRight: 10}} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
-              <Image style={{width: 100, height: 100, marginRight: 10}} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
-              <Image style={{width: 100, height: 100, marginRight: 10}} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
-              <Image style={{width: 100, height: 100, marginRight: 10}} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
-              <Image style={{width: 100, height: 100, marginRight: 10}} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
-              <Image style={{width: 100, height: 100, marginRight: 10}} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
-              <Image style={{width: 100, height: 100, marginRight: 10}} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
-              <Image style={{width: 100, height: 100, marginRight: 10}} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
-              <Image style={{width: 100, height: 100, marginRight: 10}} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
-              <Image style={{width: 100, height: 100, marginRight: 10}} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
-              <Image style={{width: 100, height: 100, marginRight: 10}} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
-              <Image style={{width: 100, height: 100, marginRight: 10}} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
-              <Image style={{width: 100, height: 100, marginRight: 10}} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
-            </ScrollView>
+            <Text style={{ color: "white", fontSize: 50, fontWeight: "bold", marginBottom: 10}}>Water</Text>
+            <View style={{borderRadius: 20}}>
+              <ScrollView style={{ backgroundColor: 'rgba(255, 255, 255, 0.1)', padding: 20, borderRadius: 25}} horizontal= {true} decelerationRate={0} snapToInterval={115} snapToAlignment={"left"}>              
+                {/* <Button onPress={() => {this.setState({duck: !this.state.duck})}} title="Press Me"/> */}
+                <Image style={styles.container} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
+                <Image style={styles.container} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
+                <Image style={styles.container} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
+                <Image style={styles.container} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
+                <Image style={styles.container} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
+                <Image style={styles.container} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
+                <Image style={styles.container} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
+                <Image style={styles.container} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
+                <Image style={styles.container} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
+                <Image style={styles.container} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
+                <Image style={styles.container} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
+                <Image style={styles.container} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
+                <Image style={styles.container} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
+                <Image style={styles.container} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
+                <Image style={styles.container} source={{uri: 'http://papermoondance.com/wp-content/uploads/2019/05/It-Must-Be-a-Duck-862x862.jpg'}}/>
+              </ScrollView>
+            </View>
           </View>
         
       </ImageBackground>
@@ -36,6 +39,10 @@ class App extends Component {
   }
 }
 
-
-
 export default App
+
+const styles = StyleSheet.create({
+  container: {
+    width: 100, height: 100, marginRight: 15, borderRadius: 20
+  }
+});
